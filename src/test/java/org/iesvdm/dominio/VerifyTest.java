3aaafbe26bf10c27e1d129e04167a1796c4021eb
jairob <jblajai1712@g.educaand.es>
2024-04-16 14:06:00 +0200

    Actualizaciones a la teoría de mockito

--- src/test/java/org/iesvdm/dominio/VerifyTest.java
+++ src/test/java/org/iesvdm/dominio/VerifyTest.java
@@ -16,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
+/* Hello world */
 public class VerifyTest {
 
     @Test
